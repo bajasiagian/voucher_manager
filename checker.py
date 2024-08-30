c5be8774637efdc2b35d2087aa733e7218b5e836
Baja Stephanus <146203885+bajasiagian@users.noreply.github.com>
2024-08-30 09:33:57 +0000

    fix stupidity typo in sheet

--- checker.py
+++ checker.py
@@ -6,7 +6,6 @@ import gspread
 import pandas as pd
 from datetime import date
 import datetime
-import numpy as np
 
 
 # Data Uploader
@@ -36,7 +35,6 @@ data_expired = expired.get_all_values()
 column_names_expired = data_expired[0]
 data_expired = data_expired[1:]
 expired_df = pd.DataFrame(data_expired, columns=column_names_expired)
-st.dataframe(expired_df)
 
 st.title("Voucher Checker ✅")
 # -------------------------------------------------#
@@ -89,37 +87,38 @@ with st.container(border=True):
     if uploaded_files is not None:
         img = Image.open(uploaded_files)
         #Check if picture is good
+        try:
+            #Read code
+            b = decode(img)
+            code = b[0].data.decode('utf-8')
+            
+            if code in db_df.voucher_id.tolist() and code not in used_df.voucher_id.tolist() and code not in expired_df.voucher_id.tolist():
+                date_diff = (datetime.datetime.strptime(db_df[db_df.voucher_id==code]['end_date'].iloc[0],"%Y-%m-%d") - datetime.datetime.today()).days
+                st.write(date_diff)
+                # Check if still valid
+                if date_diff <= 0:
+                    st.write("Expired log1")
+                    #Update expired
+                    st.write("Check 1.1")
+                    expire_update = pd.DataFrame({"voucher_id":[code],"triggered_date":[str(datetime.datetime.today())]})
+                    st.write("Expired log2")
+                    expired_df = pd.concat([expired_df,expire_update])
+                    st.write("Expired log3")
+                    expired.update([expired_df.columns.values.tolist()] + expired_df.values.tolist(), value_input_option="RAW")
 
-        #Read code
-        b = decode(img)
-        code = b[0].data.decode('utf-8')
-        
-        if code in db_df.voucher_id.tolist() and code not in used_df.voucher_id.tolist() and code not in expired_df.voucher_id.tolist():
-            date_diff = (datetime.datetime.strptime(db_df[db_df.voucher_id==code]['end_date'].iloc[0],"%Y-%m-%d") - datetime.datetime.today()).days
-            st.write(date_diff)
-            # Check if still valid
-            if date_diff <= 0:
-                st.warning("Voucher Kadaluarsa")
-                #Update expired
-                st.write("Check 1.1")
-                expire_update = pd.DataFrame({"voucher_id":[code],"triggered_date":[str(datetime.datetime.today())]})
-                st.write("Expired log2")
-                expired_df = pd.concat([expired_df,expire_update])
-                expired_df = expired_df.replace([np.inf, -np.inf], np.nan).fillna(0)
-                st.write("Expired log3")
-                expired.update([expired_df.columns.values.tolist()] + expired_df.values.tolist(), value_input_option="RAW")
+                elif date_diff > 0:
+                    discount = db_df[db_df.voucher_id==code]['discount_amount'].iloc[0]
+                    st.success(f"Voucher bisa digunakan! Diskon : {discount}")
+                    #Update used_voucher
+                    use_update = pd.DataFrame({"voucher_id":[code],"used_at":[str(datetime.datetime.today())]})
+                    used_df = pd.concat([used_df,use_update])
+                    used.update([used_df.columns.values.tolist()] + used_df.values.tolist(), value_input_option="RAW")
 
-            elif date_diff > 0:
-                discount = db_df[db_df.voucher_id==code]['discount_amount'].iloc[0]
-                st.success(f"Voucher bisa digunakan! Diskon : {discount}")
-                #Update used_voucher
-                use_update = pd.DataFrame({"voucher_id":[code],"used_at":[str(datetime.datetime.today())]})
-                used_df = pd.concat([used_df,use_update])
-                used.update([used_df.columns.values.tolist()] + used_df.values.tolist(), value_input_option="RAW")
-
-        elif code not in db_df.voucher_id.tolist():
-            st.warning("Data tidak ditemukan")
-        
-        elif code in used_df.voucher_id.tolist():
-            st.warning(f"Voucher ini telah digunakan pada {used_df[used_df.voucher_id==code]['used_at'].iloc[0]}")
-    
+            elif code not in db_df.voucher_id.tolist():
+                st.warning("Data tidak ditemukan")
+            
+            elif code in used_df.voucher_id.tolist():
+                st.warning(f"Voucher ini telah digunakan pada {used_df[used_df.voucher_id==code]['used_at'].iloc[0]}")
+            
+        except:
+            st.warning("Gagal membaca QR Code, ulang pengecekan")
